381f5484d3e5c6fd6e380a4c119a856d65fb542f
TanmayPShinde <tps.shinde@gmail.com>
2023-09-02 11:26:00 +0530

    feat: icons, styled-components package, minor

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,7 +1,12 @@
-const Container = ({sx, children}) => {
+const Container = ({ sx, children }) => {
   return (
-    <div className={`p-3 flex-grow bg-white rounded-md drop-shadow-lg ${sx}`}>{children}</div>
-  )
-}
+    <div
+      className={`p-3 flex-grow bg-white rounded-md drop-shadow-lg ${sx}`}
+      style={{ width: "380px" }}
+    >
+      {children}
+    </div>
+  );
+};
 
-export default Container
+export default Container;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,7 +6,7 @@ const Search = ({ setCity, onSearch, placeholder }) => {
     <div className="w-2/4 m-auto mb-2">
       <input
         placeholder={placeholder}
-        autoComplete={false}
+        autoComplete="off"
         onChange={(e) => setCity(e.target.value)}
         className="p-1.5 px-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
       />
