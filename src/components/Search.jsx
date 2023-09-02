5ae660bd4652fa18aec16e29ec9deb6893318cf6
TanmayPShinde <tps.shinde@gmail.com>
2023-09-02 13:31:19 +0530

    minor css

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import PrimaryButton from "./button/PrimaryButton";
 
-const Search = ({ setCity, onSearch, placeholder }) => {
+const Search = ({ setCity, onSearch, placeholder, loading }) => {
   return (
-    <div className="flex justify-center mb-2">
+    <div className="flex justify-center mb-4">
       <input
         placeholder={placeholder}
         autoComplete="off"
         onChange={(e) => setCity(e.target.value)}
-        className="p-1.5 px-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className="p-1.5 px-2 bg-gray-50 border-2  border-slate-950 text-gray-900 text-sm   "
       />
       <PrimaryButton
         text="search"
         sx="p-1.5 px-2 inline"
-        // disabled={loading}
+        disabled={loading}
         onClickHandler={onSearch}
       />
     </div>
