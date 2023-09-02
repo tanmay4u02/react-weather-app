bc4a1cdc5c72224db77b697b98f55adee9dd8e45
TanmayPShinde <tps.shinde@gmail.com>
2023-09-02 11:27:16 +0530

    feat: weather icons

---
+++ src/config/weatherIcons.js
@@ -0,0 +1,28 @@
+import sunny from "../assets/icons/sunny.svg";
+import night from "../assets/icons/night.svg";
+import day from "../assets/icons/day.svg";
+import cloudyNight from "../assets/icons/cloudy-night.svg";
+import cloudy from "../assets/icons/cloudy.svg";
+import perfectDay from "../assets/icons/perfect-day.svg";
+import rain from "../assets/icons/rain.svg";
+import rainNight from "../assets/icons/rain-night.svg";
+import storm from "../assets/icons/storm.svg";
+
+const weatherIcons = {
+  "01d": sunny,
+  "01n": night,
+  "02d": day,
+  "02n": cloudyNight,
+  "03d": cloudy,
+  "03n": cloudy,
+  "04d": perfectDay,
+  "04n": cloudyNight,
+  "09d": rain,
+  "09n": rainNight,
+  "10d": rain,
+  "10n": rainNight,
+  "11d": storm,
+  "11n": storm,
+};
+
+export default weatherIcons;
